5440ce3b1642a7d6b6628b18cfecbdd3d8c9be9b
K_Kuba <195623@edu.p.lodz.pl>
2017-05-10 20:06:38 +0200

    Tamagotchi_MVC source code.

---
+++ src/TamaC.java
@@ -0,0 +1,86 @@
+// stucture/calc-mvc/CalcController.java - Controller
+//    Handles user interaction with listeners.
+//    Calls View and Model as needed.
+// Fred Swartz -- December 2004
+
+import java.awt.event.*;
+
+public class TamaC
+{
+    //... The Controller needs to interact with both the Model and View.
+    private TamaM m_model;
+    private TamaV  m_view;
+    
+    //========================================================== constructor
+    /** Constructor */
+    TamaC(TamaM model, TamaV view)
+    {
+        m_model = model;
+        m_view  = view;
+        
+        //... Add listeners to the view.
+        view.addMultiplyListener(new MultiplyListener());
+        view.addClearListener(new ClearListener());
+        view.addPetListener( new PetListener() );
+
+      
+    }
+    
+    // -----------------------------------------
+    
+    
+    ////////////////////////////////////////// inner class MultiplyListener
+    /** When a mulitplication is requested.
+     *  1. Get the user input number from the View.
+     *  2. Call the model to mulitply by this number.
+     *  3. Get the result from the Model.
+     *  4. Tell the View to display the result.
+     * If there was an error, tell the View to display it.
+     */
+    
+    class MultiplyListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            String userInput = "";
+            try
+            {
+                userInput = m_view.getUserInput();
+                m_model.multiplyBy(userInput);
+                m_view.setTotal(m_model.getValue());
+                
+            }
+            catch (NumberFormatException nfex)
+            {
+                m_view.showError("Bad input: '" + userInput + "'");
+            }
+        }
+    }//end inner class MultiplyListener
+    
+    
+    //////////////////////////////////////////// inner class ClearListener
+    /**  1. Reset model.
+     *   2. Reset View.
+     */    
+    class ClearListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e) {
+            m_model.reset();
+            m_view.reset();
+        }
+        
+        
+    }// end inner class ClearListener
+    
+    
+    class PetListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            if( e.getSource() == m_view.returnFeedButton() )
+            {
+            	System.out.println("Pet got fed.");
+            }            
+        }    
+    }
+}
---
+++ src/TamaMVC.java
@@ -0,0 +1,20 @@
+	// structure/calc-mvc/CalcMVC.java -- Calculator in MVC pattern.
+	// Fred Swartz -- December 2004
+import javax.swing.*;
+
+public class TamaMVC
+{
+    //... Create model, view, and controller.  They are
+    //    created once here and passed to the parts that
+    //    need them so there is only one copy of each.
+    public static void main(String[] args) {
+        
+        TamaM      model      = new TamaM();
+        TamaV       view       = new TamaV(model);
+        TamaC controller = new TamaC(model, view);
+        
+        view.setVisible(true);
+    }
+}
+
+
